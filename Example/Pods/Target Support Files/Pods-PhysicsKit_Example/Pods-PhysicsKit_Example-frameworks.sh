f4e9829223ceda754e7d9c99d5f29e4fb38f6d22
Adam Eisfeld <adam.eisfeld@gmail.com>
2020-06-15 16:29:31 -0400

    - cleaning

--- Example/Pods/Target Support Files/Pods-PhysicsKit_Example/Pods-PhysicsKit_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-PhysicsKit_Example/Pods-PhysicsKit_Example-frameworks.sh
@@ -197,10 +197,10 @@ if [ -r "${ARTIFACT_LIST_FILE}" ]; then
 fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${PODS_ROOT}/../../PhysicsKit/Release/PhysicsKit.framework"
+  install_framework "${PODS_ROOT}/../../PhysicsKit/release/PhysicsKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${PODS_ROOT}/../../PhysicsKit/Release/PhysicsKit.framework"
+  install_framework "${PODS_ROOT}/../../PhysicsKit/release/PhysicsKit.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
